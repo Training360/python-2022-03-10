37437fddfb5385d49ce242185a684c8e3164c349
Istvan Viczian <viczian.istvan@gmail.com>
2022-03-10 14:27:13 +0100

    Kifejezések

---
+++ expressions.py
@@ -0,0 +1,20 @@
+print(3 ** 2)
+print(10 // 3)
+print(10 % 3)
+
+print(int(4.3))
+print(int(4.9))
+
+i = 7 + 3
+i = (7 + 3)
+i = 5 + 1 + 7
+
+x = y = z = 10
+
+i = 7 + 3 * 2
+i = (7 + 3) * 2
+i = 2
+j = 4
+i = (7 + (i * j)) * len("alma")
+
+print("X" * 3)
