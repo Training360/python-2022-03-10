8a6612ef49969805361293bda947beb57f2819ad
Istvan Viczian <viczian.istvan@gmail.com>
2022-03-10 14:41:09 +0100

    input

---
+++ console.py
@@ -0,0 +1,11 @@
+name = input("Mi a neved?")
+print(name)
+print(type(name))
+
+# age = input("Hany eves vagy?")
+# print(type(age))
+# age = int(age)
+# print(int(age) * 2)
+
+age = int(input("Hány éves vagy"))
+print(age * 2)
