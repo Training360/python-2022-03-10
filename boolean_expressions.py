7e2b5e42436425108c128b1e42e0560ae67214a5
Istvan Viczian <viczian.istvan@gmail.com>
2022-03-10 16:40:33 +0100

    Selenium

---
+++ boolean_expressions.py
@@ -0,0 +1,31 @@
+print(True)
+print(False)
+print(4 < 10)
+print(4 > 10)
+
+print(10 > 10)
+print(10 >= 10)
+
+print(10 == 10)
+i = 5
+print(i == 5)
+
+print("alma" == "alma")
+print("alma" == "korte")
+
+print("alma" < "korte")
+
+print("ábel" < "cecil")
+
+print(6 != 7)
+
+print(True or False)
+print(True and True)
+
+print(2 > 3 or 8 < 10)
+print(2 > 3 and 8 < 10)
+
+print(not 3 > 2)
+print(not "alma" == "korte")
+
+print(3 == 3.0)
