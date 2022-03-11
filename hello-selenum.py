406983d70a7055289cef87fd27e36a5cbb9fccaa
Istvan Viczian <viczian.istvan@gmail.com>
2022-03-11 09:33:20 +0100

    Áttérés Code-ra

--- hello-selenum.py
+++ hello-selenum.py
@@ -6,6 +6,6 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://python.org")
-driver.find_element(By.ID, "id-search-field").send_keys("precedence")
+driver.find_element(By.ID, "id-search-field").send_keys("Visual Studio Code")
 driver.find_element(By.ID, "submit").click()
 print("End")
