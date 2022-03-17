862d884317b4f4e92bd3c04a02f69c57b9b0c15f
Istvan Viczian <viczian.istvan@gmail.com>
2022-03-17 14:50:20 +0100

    html és konfig

--- conftest.py
+++ conftest.py
@@ -3,10 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from datetime import datetime
+from py.xml import html
+import pytest
+
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     return driver
 
 
@@ -22,3 +31,21 @@ def driver():
 #     driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
 #     driver.get("https://python.org")
 #     return driver
+
+def pytest_html_results_table_header(cells):
+    cells.insert(2, html.th("Description"))
+    cells.insert(1, html.th("Time", class_="sortable time", col="time"))
+    cells.pop()
+
+
+def pytest_html_results_table_row(report, cells):
+    cells.insert(2, html.td(report.description))
+    cells.insert(1, html.td(datetime.utcnow(), class_="col-time"))
+    cells.pop()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    report.description = str(item.function.__doc__)
