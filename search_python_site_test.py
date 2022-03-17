5dbb7872c720698212756dc5723546a2d49bdf16
Istvan Viczian <viczian.istvan@gmail.com>
2022-03-17 14:49:56 +0100

    HTML riport és konfig

--- search_python_site_test.py
+++ search_python_site_test.py
@@ -1,18 +1,24 @@
+from urllib.parse import urljoin
+import pytest
 from selenium.webdriver.common.by import By
 
 from selenium.webdriver.remote.webdriver import WebDriver
 
+@pytest.mark.fn_search
+@pytest.mark.basic
 def test_search_pep(driver: WebDriver, base_url):
+    """This test case is for search input field."""
     driver.get(base_url)
     driver.find_element(By.ID, "id-search-field").send_keys("pep")
     driver.find_element(By.ID, "submit").click()
+
 
     first_link = driver.find_element(By.CSS_SELECTOR, "#content > div > section > form > ul > li:nth-child(1) > h3 > a").text
     assert "pep" in first_link.lower()
 
 def test_about(driver, base_url):
     # driver.find_element(By.LINK_TEXT, "About").click()
-    driver.get(base_url + "/about/")
+    driver.get(urljoin(base_url, "/about/"))
 
     title = driver.title
     assert "about" in title.lower()
